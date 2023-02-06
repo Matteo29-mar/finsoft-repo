6fde26885e17ce54685d6ae57acda61599f3a27a
matteo29-mar <matteomarziano.mm42@gmail.com>
2023-02-06 15:53:57 +0100

    server.js prova

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const hostname = 'localhost';
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+res.statusCode = 200;
+
+res.setHeader('Content-Type', 'text/plain');
+
+res.end('Hello World, howtoforge');
+
+});
+
+server.listen(port, hostname, () => {
+
+console.log(`Server running at http://${hostname}:${port}/`);
+
+})
