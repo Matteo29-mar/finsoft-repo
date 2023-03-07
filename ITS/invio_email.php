ddcbda3055fbff3914b43f415ba9e644e95e8215
Matteo29-mar <matteomarziano.mm42@gmail.com>
2023-03-07 15:01:02 +0100

    UPDATE

--- ITS/invio_email.php
+++ ITS/invio_email.php
@@ -15,19 +15,19 @@ if ($result->num_rows > 0) {
   // Recupera tutti i titoli e le descrizioni dalla tabella 'privati'
   $sql_privati = "SELECT email,titolo, descrizione FROM privati ORDER BY id_privato DESC LIMIT 1"; //seleziona l'ultima riga creata 
   $result_privati = $conn->query($sql_privati);
-  $rows_privati = $result_privati->fetch_all(MYSQLI_ASSOC); 
+  $rows_privati = $result_privati->fetch_all(MYSQLI_ASSOC);
   /*utilizza la funzione fetch_all all'oggetto mysqli_assoc 
   per recuperare tutte le righe di una query 
   e un array associativo contentente tutti i record*/
   // Ciclo su ogni riga di risultati della query per recuperare gli indirizzi email delle aziende
-  while($row = $result->fetch_assoc()) {
+  while ($row = $result->fetch_assoc()) {
     // Recupera l'email dell'azienda corrente
-        $email = $row["email"];
-        $template = file_get_contents("recover.html");
-        $search = ["{{NAME}}","{{RESET_URL}}"];
-        $replace =["azienda",HTTP_BASEURL. "ITS/azienda.php"];
-        $body =str_replace($search, $replace, $template);
-        send_mail($email, "Nuovo messaggio dall'azienda",  $body);
+    $email = $row["email"];
+    $template = file_get_contents("recover.html");
+    $search = ["{{NAME}}", "{{RESET_URL}}"];
+    $replace = ["azienda", HTTP_BASEURL . "ITS/azienda.php"];
+    $body = str_replace($search, $replace, $template);
+    send_mail($email, "Nuovo messaggio dall'azienda",  $body);
   }
 } else {
   echo "Nessun risultato trovato.";
@@ -35,5 +35,3 @@ if ($result->num_rows > 0) {
 // Chiudi la connessione al database
 mysqli_close($conn);
 ?>
-
-
